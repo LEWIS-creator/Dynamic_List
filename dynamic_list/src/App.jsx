7fc5551b1138d931f32f3a3995cf72e15497b7f9
LEWIS MWILITSA <Lewismwilitsa00@gmail.com>
2025-04-08 18:43:27 +0000

    dynamic_list

---
+++ dynamic_list/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LewisList from "./components/lewis";
+
+const App = () => {
+  return (
+      <div>
+            <LewisList />
+                </div>
+                  );
+                  };
+
+                  export default App;
